ecfb29625c8fef0aee1643519c5699f440fa7e06
Gabriel <gabrielruss88@gmail.com>
2018-02-16 15:24:19 -0500

    working so far

--- src/components/course/coursesPage.jsx
+++ src/components/course/coursesPage.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import { Input, Button } from 'semantic-ui-react';
+import * as courseActions from '../../actions/courseActions';
 
 class CoursesPage extends React.Component {
   constructor(props, context) {
@@ -11,21 +13,25 @@ class CoursesPage extends React.Component {
   }
 
   onClickSave = () => {
-    alert(`Saving ${this.state.course.title}`);
+    this.props.dispatch(courseActions.createCourse(this.state.course));
   }
 
   onTitleChange = (event) => {
     const course = this.state.course;
     course.title = event.target.value;
     this.setState({ course: course });
-  };
+  }
+
+  courseRow = (course, index) => {
+    return <div key={index}>{course.title}</div>;
+  }
 
   render() {
     return (
       <div>
         <h1>Courses</h1>
+        {this.props.courses.map(this.courseRow)}
         <h2>Add Course</h2>
-
         <Input
           onChange={this.onTitleChange}
           value={this.state.course.title} />
@@ -38,4 +44,10 @@ class CoursesPage extends React.Component {
   }
 }
 
-export default CoursesPage;
+function mapStateToProps(state, ownProps) {
+  return {
+    courses: state.courses
+  };
+}
+
+export default connect(mapStateToProps)(CoursesPage);
