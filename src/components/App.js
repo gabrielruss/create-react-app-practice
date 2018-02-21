e9bd1a49a6f79cf1e71c204c2e57cdfad128e789
Gabriel <gabrielruss88@gmail.com>
2018-02-21 11:25:23 -0500

    going to switch to updating my app instead

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import HomePage from './home/HomePage';
 import AboutPage from './about/AboutPage';
 import { Switch, Route } from 'react-router-dom';
 import CoursesPage from './course/CoursesPage';
+import ManageCoursePage from './course/ManageCoursePage';
 
 class App extends React.Component {
   render() {
@@ -13,6 +14,8 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/courses' component={CoursesPage} />
+          <Route path='/course' component={ManageCoursePage} />
+          <Route path='/course/:id' component={ManageCoursePage} />
           <Route path='/about' component={AboutPage} />
         </Switch>
       </div>
